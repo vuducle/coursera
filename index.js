904c372f86a651e5f238229c6b3d0fcd2058a12a
Vu Duc Le <97.vuducle@gmail.com>
2022-06-15 19:34:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const bodyParser = require('body-parser');
+const express = require('express'),
+     http = require('http');
+const dishRouter = require('./Router/DishRouter')
+const morgan = require('morgan');
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.use('/dishes', dishRouter);
+app.get('/dishes/:dishId', (req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId + '\n');
+  res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+app.use(express.static(__dirname + '/public'));
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

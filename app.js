c2eead0572c20a82e3118d09ae86226cf6206be1
Minh Le <vuminhle@outlook.com>
2022-06-17 15:16:54 +0200

    second commit 2

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const dishRouter = require('./routes/DishRouter')
+const promoRouter = require('./routes/PromoRouter')
+const leaderRouter = require('./routes/LeaderRouter')
+
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter.dishRouter);
+app.use('/dishes', dishRouter.dishRouterId);
+app.use('/promotions', promoRouter.promoRouter);
+app.use('/promotions', promoRouter.promoRouterId);
+app.use('/leaders', leaderRouter.leaderRouter);
+app.use('/leaders', leaderRouter.leaderRouterId);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

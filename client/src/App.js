ea029073d53a42f3d69494baad668244d250b8cd
Vu Duc Le <97.vuducle@gmail.com>
2022-08-01 14:41:07 +0200

    chakra ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Dishes from "./Components/Dishes";
-
+import { ChakraProvider } from '@chakra-ui/react'
 function App() {
     return (
-      <div>
-        <Dishes></Dishes>
-      </div>
-    
+      <ChakraProvider>
+        <div>
+          <Dishes></Dishes>
+        </div>
+      </ChakraProvider>
   );
 }
 
